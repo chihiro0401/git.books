aee24e59e7fc8d273dec0aa78191b1ae4f361a6c
EC2 Default User <ec2-user@ip-172-31-19-98.ap-northeast-1.compute.internal>
2023-04-15 06:41:48 +0000

    [Finish]Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,51 +1,48 @@
 class BooksController < ApplicationController
-  
-    def index
-  @books = Book.all
-  @book = Book.new
+
+  def index
+   @books = Book.all
+   @book = Book.new
   end
 
  def create
    @book = Book.new(book_params)
    if  @book.save
-   flash[:notice] = "Book was successfully destroyed."
+   flash[:notice] = "Book was successfully created."
    redirect_to book_path(@book.id)
  else
-   @book = Book.all
+   @books = Book.all
    render :index
    end
  end
-  
+
   def show
-   @book = Book.find(params[:id]) 
+   @book = Book.find(params[:id])
   end
-  
-  
+
+
  def edit
-    @book = Book.new(book_params)
-    if @book.save
-    flash[:notice] = "Book was successfully destroyed."
+   @book = Book.find(params[:id])
+
+ end
+
+
+
+ def update
+     @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
     redirect_to book_path(@book.id)
   else
-   @book = Book.all
-   render :index
+   render :edit
   end
  end
- 
 
-  
-   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
-    flash[:notice] = "Book was successfully destroyed."
-  end
-  
   def destroy
     book = Book.find(params[:id])
     book.destroy
     flash[:notice] = "Book was successfully destroyed."
-    redirect_to  '/books'  
+    redirect_to  '/books'
   end
 
 
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ApplicationRecord
   
-  validates :title, presence: true
-  validates :body, presence: true
+  validates :title, presence: true ,uniqueness: true
+
+  validates :body, presence: true ,uniqueness: true
 end
